b1a774908e7dcbd834b63896d7ebbc974c9f1a67
tproduit <timothee.produit@gmail.com>
2019-06-25 11:57:20 +0200

    add infiltration

---
+++ swmm_views/vw_swmm_infiltration.sql
@@ -0,0 +1,17 @@
+DROP VIEW IF EXISTS qgep_swmm.vw_infiltration;
+
+--------
+-- View for the swmm module class junction
+-- 20190329 qgep code sprint SB, TP
+--------
+
+CREATE OR REPLACE VIEW qgep_swmm.vw_infiltration AS
+
+SELECT 
+	ca.obj_id as Subcatchment,
+	3 as MaxRate,
+	0.5 as MinRate,
+	4 as Decay,
+	7 as DryTime,
+	0 as MaxInfil
+FROM qgep_od.catchment_area as ca
